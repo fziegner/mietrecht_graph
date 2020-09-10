9ff43612c7f24a5f48fe109cd90ee4935ee530c9
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2020-09-10 17:30:50 +0200

    Improved JuraBasic spider formatting

--- BauGraph/bauprofessor/spiders/JuraBasicSpider.py
+++ BauGraph/bauprofessor/spiders/JuraBasicSpider.py
@@ -4,18 +4,35 @@ import re
 class JuraBasicItem(scrapy.Item):
     page_url = scrapy.Field()
     title = scrapy.Field()
+    sub_title = scrapy.Field()
     text = scrapy.Field()
     crosslinks = scrapy.Field()
     category = scrapy.Field()
 
 class JuraBasicSpider(scrapy.Spider):
 
+    #links = Response.xpath('//*[@class="button_details"]/@href').getall()
+    #start_urls = ["http://www.jura-basic.de/" + link for link in links]
     name = "JuraBasic"
-    start_urls = ['http://www.jura-basic.de/aufruf.php?file=&find=Mietvertrag']
+    start_urls = ['http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1468yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1488yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_742yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1067yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1066yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_698yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1446yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1145yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_219yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1663yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_1525yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_611yxymietrecht',
+                     'http://www.jura-basic.de/aufruf.php?file=&pp=&art=6&find=t_89yxymietrecht']
+    custom_settings = {
+        'DEPTH_LIMIT': 2
+    }
 
     def parse(self, response):
 
-        crawled_links = []
         links = list(set(response.xpath('//*[@class="text_inhalt"]/a/@href').getall() + response.xpath('//*[@class="kurzue_links"]/@href').getall()))
         links = [link for link in links if ("jura-basic" and "file=1") in link]
 
@@ -26,27 +43,34 @@ class JuraBasicSpider(scrapy.Spider):
             else:
                 cleaned_links.append(link)
 
-        for link in cleaned_links:
-            if link not in crawled_links:
-                crawled_links.append(link)
-                yield scrapy.Request(link, callback=self.parse)
-            else:
-                yield scrapy.Request(link, callback=self.parseText)
-
-    def parseText(self, response):
-
         element = response.xpath('//*[@class="text_titel"]')
 
-        if len(element) == 1:
+        if len(element) == 1 and (str(response.url) not in JuraBasicSpider.start_urls):
             tmp_item = JuraBasicItem()
             tmp_item['page_url'] =  str(response.url)
-            tmp_item['title'] = response.xpath('//*[@class="text_titel"]/text()').get().strip() + " - " + response.xpath('//*[@class="text_inhalt"]/h2/text()').get().strip()
-            tmp_item['text'] = re.sub("[\r]","",response.xpath('string(//*[@class="text_inhalt"])').get()).strip()
-            tmp_item['crosslinks'] = set(response.xpath('//*[@class="text_inhalt"]/a/@href').extract())
-            tmp_item['category'] = response.xpath('//*[@class="text_titel"]/text()').get()
+            tmp_item['title'] = response.xpath('//*[@class="text_titel"]/text()').get().strip()
+            if(response.xpath('//*[@class="text_inhalt"]/h2/text()').get()):
+                tmp_item['sub_title'] = response.xpath('//*[@class="text_inhalt"]/h2/text()').get().strip()
+            if "sub_title" in tmp_item:
+                tmp = re.sub("[\r]","",response.xpath('string(//*[@class="text_inhalt"])').get()).strip()
+                tmp_item['text'] = re.split("(<< )?(\|\| *)?(Rz. [0-9][0-9]?)",tmp)[4].strip()
+            else:
+                tmp_item['text'] = re.sub("[\r]","",response.xpath('string(//*[@class="text_inhalt"])').get()).strip()
+            crosslinks = list(set(response.xpath('//*[@class="text_inhalt"]/a/@href').getall()))
+            cleaned_crosslinks = []
+            for link in crosslinks:
+                if link.startswith("//"):
+                    cleaned_crosslinks.append("http:" + link)
+                else:
+                    cleaned_crosslinks.append(link)
+            tmp_item['crosslinks'] = cleaned_crosslinks
+            #tmp_item['category'] = response.xpath('//*[@class="text_titel"]/text()').get()
             yield tmp_item
         else:
             print("Nothing to Scrape!")
+
+        for link in cleaned_links:
+            yield scrapy.Request(link, callback=self.parse)
 
         #filename = 'quotes-%s.html' % page
         #with open(filename, 'wb') as f:
