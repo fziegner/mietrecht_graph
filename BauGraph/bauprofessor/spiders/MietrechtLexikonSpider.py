90d638b4883345f4042bc11c725c0da9a3f0ab52
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2020-09-30 16:47:45 +0200

    Improved MietrechtLexikon spider

--- BauGraph/bauprofessor/spiders/MietrechtLexikonSpider.py
+++ BauGraph/bauprofessor/spiders/MietrechtLexikonSpider.py
@@ -6,6 +6,7 @@ class MietrechtLexikonItem(scrapy.Item):
     title = scrapy.Field()
     text = scrapy.Field()
     crosslinks = scrapy.Field()
+    sub_title = scrapy.Field()
 
 class MietrechtLexikonSpider(scrapy.Spider):
 
@@ -34,21 +35,25 @@ class MietrechtLexikonSpider(scrapy.Spider):
                 tmp_item['page_url'] =  str(response.url).replace("https","http")
             else:
                 tmp_item['page_url'] =  str(response.url)
-            tmp_item['title'] = response.xpath('//*[@class="column one"]/h1/text()').get()
+            if response.xpath('//*[@class="column one"]/h1/text()').get().startswith("Mietrecht: "):
+                tmp_item['title'] = response.xpath('//*[@class="column one"]/h1/text()').get().split("Mietrecht: ", 1)[1]
+            else:
+                tmp_item['title'] = response.xpath('//*[@class="column one"]/h1/text()').get()
+
+            if response.xpath('string(//*[@class="the_content_wrapper"]/h2)').get().startswith("Mietrecht: "):
+                tmp_item['sub_title'] = response.xpath('string(//*[@class="the_content_wrapper"]/h2)').get().split("Mietrecht: ", 1)[1]
+            else:
+                tmp_item['sub_title'] = response.xpath('string(//*[@class="the_content_wrapper"]/h2)').get()
             sub_title = response.xpath('string(//*[@class="the_content_wrapper"]/h2)').get()
+
             if sub_title:
                 if(response.xpath('//*[@class="toc_title"]/text()').get()):
-                    tmp_item['text'] = sub_title + " " + response.xpath('string(//*[@class="the_content_wrapper"])').get().split(sub_title)[2]
+                    tmp_item['text'] = response.xpath('string(//*[@class="the_content_wrapper"])').get().split(sub_title)[2]
                 else:
-                    tmp_item['text'] = sub_title + " " + response.xpath('string(//*[@class="the_content_wrapper"])').get().split(sub_title)[1]
+                    tmp_item['text'] = response.xpath('string(//*[@class="the_content_wrapper"])').get().split(sub_title)[1]
             else:
                 tmp_item['text'] = response.xpath('string(//*[@class="the_content_wrapper"])').get()
             tmp_item['crosslinks'] = response.xpath('//*[@class="the_content_wrapper"]/*[@class="schriften"]/a/@href').extract()
             yield tmp_item
         else:
             print("Nothing to Scrape!")
-
-        #filename = 'quotes-%s.html' % page
-        #with open(filename, 'wb') as f:
-        #    f.write(response.body)
-        #self.log('Saved file %s' % filename)
