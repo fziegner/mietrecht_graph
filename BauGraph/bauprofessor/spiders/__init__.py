0bdf0f5a2858c334bf7845255990f4697026af0b
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2020-10-01 17:52:03 +0200

    Updated init method for spiders

--- BauGraph/bauprofessor/spiders/__init__.py
+++ BauGraph/bauprofessor/spiders/__init__.py
@@ -3,25 +3,18 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 
-from .BauprofessorSpider import BauprofessorSpider
-from .beuth_crawler import BeuthLexSpider
-from .beuth_din_crawler import BeuthDINSpider
-from .beuth_vob_crawler import BeuthVOBSpider
-from .HausberaterSpider import HausberaterSpider
-from .HOAI import HOAISpider
-
 from .JuraBasicSpider import JuraBasicSpider
 from .MietrechtEinfachSpider import MietrechtEinfachSpider
 from .MietrechtLexikonSpider import MietrechtLexikonSpider
+from .BGBSpider import BGBSpider
+from .BMGEVSpider import BMGEVSpider
+from .RechtslexikonSpider import RechtslexikonSpider
 
 Spiders = [
-    BauprofessorSpider,
-    BeuthLexSpider,
-    BeuthDINSpider,
-    BeuthVOBSpider,
-    HausberaterSpider,
-    HOAISpider,
     JuraBasicSpider,
     MietrechtEinfachSpider,
-    MietrechtLexikonSpider
+    MietrechtLexikonSpider,
+    BGBSpider,
+    BMGEVSpider,
+    RechtslexikonSpider
 ]
